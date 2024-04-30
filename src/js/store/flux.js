04dbe6292cdaff94c6c6c20251beba54ab9d0ee5
COCOASOO <85440064+COCOASOO@users.noreply.github.com>
2024-04-30 10:21:11 +0000

    first commit

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -6,34 +6,66 @@ const getState = ({ getStore, getActions, setStore }) => {
 				name: "",
 				url: ""
 			},
+
 			infoSelectedCharacter: {
-				result: {
-					height: "",
-					mass: "",
-					hair_color: "",
-					skin_color: "",
-					eye_color: "",
-					birth_year: "",
-					gender: "",
-					created: "",
-					edited: "",
-					name: "",
-					homeworld: "",
-					url: ""
-				},
-				description: ""
+				height: "",
+				mass: "",
+				hair_color: "",
+				skin_color: "",
+				eye_color: "",
+				birth_year: "",
+				gender: "",
+				created: "",
+				edited: "",
+				name: "",
+				homeworld: "",
+				url: ""
 			},
+
 			selectedPlanet: {
-
+				uid: "",
+				name: "",
+				url: ""
 			},
+
 			infoSelectedPlanet: {
-
+				diameter: "",
+				rotation_period: "",
+				orbital_period: "",
+				gravity: "",
+				population: "",
+				climate: "",
+				terrain: "",
+				surface_water: "",
+				created: "",
+				edited: "",
+				name: "",
+				url: ""
 			},
+
 			selectedVehicle: {
-
+				uid: "",
+				name: "",
+				url: ""
 			},
-			infoSelectedVehicle: {
 
+			infoSelectedVehicle: {
+				model: "",
+				vehicle_class: "",
+				manufacturer: "",
+				cost_in_credits: "",
+				length: "",
+				crew: "",
+				passengers: "",
+				max_atmosphering_speed: "",
+				cargo_capacity: "",
+				consumables: "",
+				films: [],
+				pilots: [],
+				created: "",
+				edited: "",
+				name: "",
+				url: ""
 			}
 		},
 		actions: {
