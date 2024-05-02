9b387edc68557416f4817fe1cbc64397711680ef
COCOASOO <85440064+COCOASOO@users.noreply.github.com>
2024-05-02 23:03:13 +0000

    carruseh hehco

--- src/js/component/genericCard.js
+++ src/js/component/genericCard.js
@@ -3,11 +3,10 @@ import { images } from "../utils";
 
 export const GenericCard = ({ body }) => {
     const imageName = body.name.replaceAll(" ", "_");
-    console.log(imageName);
     const imagePath = images[imageName];
-    console.log(imagePath);
+
     return (
-        <div className="card">
+        <div className="card" onClick={()=>{console.log("hola")}}>
             <img src={imagePath} className="card-img-top" alt={body.name} />
             <div className="card-body">
                 <h5 className="card-title text-black">{body.name}</h5>
